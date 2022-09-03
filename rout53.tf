740a6690f16e2126912e478251b54fe7cebb8e66
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 08:30:05 +0530

    Auto Push Commit

--- rout53.tf
+++ rout53.tf
@@ -1,8 +1,12 @@
-resource "aws_route53_record" "redis-record" {
-    # I'd need the zone details, which we have given in the vpc module, so we need to fetch the zone id from there. We are can read remote state from their outputs
-  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
-  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
-  type    = "CNAME"
-  ttl     = 660
-  records = [aws_elasticache_cluster.redis.endpoint]
+# resource "aws_route53_record" "redis-record" {
+#     # I'd need the zone details, which we have given in the vpc module, so we need to fetch the zone id from there. We are can read remote state from their outputs
+#   zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
+#   name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
+#   type    = "CNAME"
+#   ttl     = 660
+#   records = [aws_elasticache_cluster.redis.endpoint]
+# }
+
+output "redis" {
+    value = 
 }
